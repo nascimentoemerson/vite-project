ac24e60833bd24296db42ec4e820dabf22c086ad
Emerson Nascimento <106358298+nascimentoemerson@users.noreply.github.com>
2023-01-25 18:32:18 -0300

    feat: create api base

--- src/utils/api/api.ts
+++ src/utils/api/api.ts
@@ -1,26 +1,56 @@
 import axios from "axios";
 import { LoginRequest } from "../types/requests";
 
-axios.defaults.baseURL = 'http://localhost:3000'
-axios.defaults.headers.post["Content-Type"] = "application/json"
+axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
-axios.interceptors.request.use(function (config) {
-    // Faz alguma coisa antes da requisição ser enviada
+axios.interceptors.request.use(
+  function (config) {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers = {
+        Authorization: "Bearer " + token,
+      };
+    }
     return config;
-  }, function (error) {
-    // Faz alguma coisa com o erro da requisição
+  },
+  function (error) {
     return Promise.reject(error);
-  });
+  }
+);
+
+axios.interceptors.response.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    if (error.response.status === 401) {
+      if (localStorage.getItem("token")) localStorage.removeItem("token");
+    }
+    throw new Error(error.response.data.message);
+  }
+);
+
+
 
 
 export const api = {
     login: async ({email, password} :LoginRequest) => {
         try {
             const response = await axios.post('/Authorization/login', {email, password})
-            localStorage.setItem('token', response.data.token)
+            localStorage.setItem("token", response.data.token)
             return response.data
         } catch (err) {
             alert(err)
         }
+    },
+
+    getClassrooms: async () => {
+    try {
+      const response = await axios.get("/classroom");
+      return response.data;
+    } catch (err) {
+        alert(err)
     }
+  },
 }
