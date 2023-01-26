b66e211f791fb863f6798e621819219962f65dc2
Emerson Nascimento <106358298+nascimentoemerson@users.noreply.github.com>
2023-01-25 21:17:21 -0300

    feat: add axios make http requests

--- src/components/moleculas/login-form/login-form.tsx
+++ src/components/moleculas/login-form/login-form.tsx
@@ -1,42 +1,39 @@
 import { FormEvent, useState } from "react";
 import { StyledForm, StyledLoginForm } from "./styles";
-import { BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs";
+import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
 import { api } from "../../../utils/api/api";
 import { useNavigate } from "react-router-dom";
 import { Loading } from "../loading/loading";
 
-
 export function LoginForm() {
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<boolean>(false);
 
-    const [showPassword, setShowPassword] = useState<boolean>(false)
-    const [loading, setLoading] = useState<boolean>(false)
-    const [error, setError] = useState<boolean>(false)
-
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     function handleShowPassword() {
-        setShowPassword(!showPassword)
+        setShowPassword(!showPassword);
     }
 
     async function handleSubmit(e: FormEvent<HTMLFormElement>) {
-        e.preventDefault()
-        setLoading(true)
+        e.preventDefault();
+        setLoading(true);
 
         const loginPayload = {
             email: e.currentTarget.email.value,
             password: e.currentTarget.password.value,
         };
-
-        const userData = await api.login(loginPayload)
-        setLoading(false)
-        console.log('user-data', userData)
+        const userData = await api.login(loginPayload);
+        setLoading(false);
+        console.log(userData);
         if (!userData) {
-            setError(true)
-            return
-        } else {
-            navigate('/classroom')
+            setError(true);
+            return;
         }
+        navigate("/classroom");
     }
+
     return (
         <>
             {loading ? (
@@ -47,16 +44,24 @@ export function LoginForm() {
                     <StyledForm onSubmit={handleSubmit} error={error}>
                         <input placeholder="Seu email" name="email" required />
                         <div>
-                            <input placeholder="Sua senha" type={showPassword ? "text" : "password"} name="password" required />
-                            <button onClick={handleShowPassword}>
-                                {showPassword ? <BsFillEyeSlashFill /> : <BsFillEyeFill />}
+                            <input
+                                placeholder="Sua senha"
+                                type={showPassword ? "text" : "password"}
+                                name="password"
+                                required
+                            />
+                            <button type="button" onClick={handleShowPassword}>
+                                {showPassword ? (
+                                    <BsEyeSlashFill size={25} />
+                                ) : (
+                                    <BsEyeFill size={25} />
+                                )}
                             </button>
                         </div>
-                        <button type='submit'>Login</button>
+                        <button type="submit">Login</button>
                     </StyledForm>
                 </StyledLoginForm>
-            )
-            }
+            )}
         </>
     );
 }
